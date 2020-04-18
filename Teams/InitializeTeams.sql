3c529e1b79a707bc4455255d54eddc155125293a
T <43817374+tyler-huffman@users.noreply.github.com>
2020-04-18 17:25:34 -0400

    Broke apart the SQL file into smaller SQL files inside of folders representing the tables they interact with

---
+++ Teams/InitializeTeams.sql
@@ -0,0 +1,12 @@
+-- Add Orgs: 
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (1 , 'Team Liquid', 1);
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (2 , 'Cloud9', 1);
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (3 , '100 Thieves', 1);
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (4 , 'Team Dignitas', 1);
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (5 , 'Flyquest', 1);
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (6 , 'Counter Logic Gaming', 1);
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (7 , 'Evil Geniuses', 1);
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (8 , 'Golden Guardians', 1);
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (9 , 'Immortals', 1);
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (10, 'Team SoloMid', 1);
+
