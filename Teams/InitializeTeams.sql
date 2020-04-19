eecd0eb711495b819ec3b783741ed0fe08b44d92
T <43817374+tyler-huffman@users.noreply.github.com>
2020-04-18 22:06:38 -0400

    Added NA Academy teams

--- Teams/InitializeTeams.sql
+++ Teams/InitializeTeams.sql
@@ -1,12 +1,25 @@
--- Add Orgs: 
-INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (1 , 'Team Liquid', 1);
-INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (2 , 'Cloud9', 1);
-INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (3 , '100 Thieves', 1);
-INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (4 , 'Team Dignitas', 1);
-INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (5 , 'Flyquest', 1);
-INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (6 , 'Counter Logic Gaming', 1);
-INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (7 , 'Evil Geniuses', 1);
-INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (8 , 'Golden Guardians', 1);
-INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (9 , 'Immortals', 1);
-INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (10, 'Team SoloMid', 1);
+-- Add Teams:
+/*
+	
+*/
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (1,  'Team Liquid'         , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (2,  'Cloud9'              , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (3,  '100 Thieves'         , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (4,  'Team Dignitas'       , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (5,  'FlyQuest'            , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (6,  'Counter Logic Gaming', 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (7,  'Evil Geniuses'       , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (8,  'Golden Guardians'    , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (9,  'Immortals'           , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (10, 'Team SoloMid'        , 1 );
 
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (11, 'Team Liquid Academy'     , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (12, 'Cloud9 Academy'          , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (13, '100 Thieves Academy'     , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (14, 'Dignitas Academy'        , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (15, 'FlyQuest Academy'        , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (16, 'CLG Academy'             , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (17, 'Evil Geniuses Academy'   , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (18, 'Golden Guardians Academy', 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (19, 'Immortals Academy'       , 1 );
+INSERT INTO Teams (TeamID, TeamName, CountryID ) VALUES (20, 'TSM Academy'             , 1 );
