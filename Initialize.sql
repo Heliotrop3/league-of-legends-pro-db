c22a243614db904ecf9342b6f2aa4f7c4915ea23
T <43817374+tyler-huffman@users.noreply.github.com>
2020-05-21 23:06:22 -0400

    Changed all references to the Country table to the Regions table and added the information about the current professional league scene.

--- Initialize.sql
+++ Initialize.sql
@@ -1,16 +1,25 @@
 -- Add the countries
-INSERT INTO Countries (CountryID, CountryName, CountryAbbrv) VALUES (1,'United States', 'NA');
-INSERT INTO Countries (CountryID, CountryName, CountryAbbrv) VALUES (2,'Europe', 'EU');
-INSERT INTO Countries (CountryID, CountryName, CountryAbbrv) VALUES (3,'South Korea', 'KR');
-INSERT INTO Countries (CountryID, CountryName, CountryAbbrv) VALUES (4,'China', 'CN');
-INSERT INTO Countries (CountryID, CountryName, CountryAbbrv) VALUES (5,'Southeast Asia', 'PCS');  --Specifically Taiwan, Hong Kong, Macau, and Southeast Asia minus Veitnam
-INSERT INTO Countries (CountryID, CountryName, CountryAbbrv) VALUES (6,'Vietnam', 'VN');
-INSERT INTO Countries (CountryID, CountryName, CountryAbbrv) VALUES (7,'Russia', 'CIS');          --Formally known as the Commonwealth of Independent States
-INSERT INTO Countries (CountryID, CountryName, CountryAbbrv) VALUES (8,'Turkey', 'TR');
-INSERT INTO Countries (CountryID, CountryName, CountryAbbrv) VALUES (9,'Brazil', 'BR');
-INSERT INTO Countries (CountryID, CountryName, CountryAbbrv) VALUES (10,'Latin America', 'LAT');
-INSERT INTO Countries (CountryID, CountryName, CountryAbbrv) VALUES (11,'Japan', 'JP');
-INSERT INTO Countries (CountryID, CountryName, CountryAbbrv) VALUES (12,'Oceania', 'OCE');
+INSERT INTO Regions (RegionID, RegionName, RegionAbbrv) VALUES (1,'United States', 'NA');
+INSERT INTO Regions (RegionID, RegionName, RegionAbbrv) VALUES (2,'Europe', 'EU');
+INSERT INTO Regions (RegionID, RegionName, RegionAbbrv) VALUES (3,'South Korea', 'KR');
+INSERT INTO Regions (RegionID, RegionName, RegionAbbrv) VALUES (4,'China', 'CN');
+INSERT INTO Regions (RegionID, RegionName, RegionAbbrv) VALUES (5,'Southeast Asia', 'PCS');  --Specifically Taiwan, Hong Kong, Macau, and Southeast Asia minus Veitnam
+INSERT INTO Regions (RegionID, RegionName, RegionAbbrv) VALUES (6,'Vietnam', 'VN');
+INSERT INTO Regions (RegionID, RegionName, RegionAbbrv) VALUES (7,'Russia', 'CIS');          --Formally known as the Commonwealth of Independent States
+INSERT INTO Regions (RegionID, RegionName, RegionAbbrv) VALUES (8,'Turkey', 'TR');
+INSERT INTO Regions (RegionID, RegionName, RegionAbbrv) VALUES (9,'Brazil', 'BR');
+INSERT INTO Regions (RegionID, RegionName, RegionAbbrv) VALUES (10,'Latin America', 'LAT');
+INSERT INTO Regions (RegionID, RegionName, RegionAbbrv) VALUES (11,'Japan', 'JP');
+INSERT INTO Regions (RegionID, RegionName, RegionAbbrv) VALUES (12,'Oceania', 'OCE');
+
+-- Add the Pro Leagues
+INSERT INTO Leagues (LeagueID, RegionID, LeagueName, LeagueAbbrv) VALUES (1,1,"League of Legends Champion Series", "LCS");
+INSERT INTO Leagues (LeagueID, RegionID, LeagueName, LeagueAbbrv) VALUES (2,1,"League of Legends Champion Series Academy", "LCS.A");
+INSERT INTO Leagues (LeagueID, RegionID, LeagueName, LeagueAbbrv) VALUES (3,9,"Campeonato Brasileiro de League of Legends", "CBLOL");
+INSERT INTO Leagues (LeagueID, RegionID, LeagueName, LeagueAbbrv) VALUES (4,2,"League of Legends European Championship", "LEC");
+INSERT INTO Leagues (LeagueID, RegionID, LeagueName, LeagueAbbrv) VALUES (5,5,"Pacific Championship Series", "PCS");
+INSERT INTO Leagues (LeagueID, RegionID, LeagueName, LeagueAbbrv) VALUES (6,3,"League of Legends Champions Korea", "LCK");
+INSERT INTO Leagues (LeagueID, RegionID, LeagueName, LeagueAbbrv) VALUES (7,8,"Turkish Championship League", "TCL");
 
 /* Add the Roles 
 
@@ -23,3 +32,5 @@ INSERT INTO Positions (PositionID, PositionName, PositionAbbrv) VALUES (2,'Suppo
 INSERT INTO Positions (PositionID, PositionName, PositionAbbrv) VALUES (3,'Mid Laner', 'Mid' );
 INSERT INTO Positions (PositionID, PositionName, PositionAbbrv) VALUES (4,'Top Laner', 'Top' );
 INSERT INTO Positions (PositionID, PositionName, PositionAbbrv) VALUES (5,'Jungler'  , 'Jng' );
+
+
